9324023d8d62479358f541e05d3f12f313076c22
jeborsel <jeborsel@gmail.com>
2017-09-27 08:36:54 +0000

    added IE11/Edge support

--- src/main.js
+++ src/main.js
@@ -6,9 +6,9 @@ require(`./themes/app.${__THEME}.styl`)
 // require(`quasar/dist/quasar.${__THEME}.css`)
 // ==============================
 
-// Uncomment the following lines if you need IE11/Edge support
-// require(`quasar/dist/quasar.ie`)
-// require(`quasar/dist/quasar.ie.${__THEME}.css`)
+
+require(`quasar/dist/quasar.ie`)
+require(`quasar/dist/quasar.ie.${__THEME}.css`)
 
 import Vue from 'vue'
 import Quasar from 'quasar'
